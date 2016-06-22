0dff7075decea69aaa7005a4baa86b11788ec252
rufusderrid <rufusderrid@gmail.com>
2016-06-22 16:23:40 +0300

    d

---
+++ AISystem/StateMachine.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIPract.AISystem
+{
+    public class StateMachine<T>
+    {
+        private T _owner;
+
+        private State<T> _currentState;
+        private State<T> _previousState;
+        private State<T> _globalState;
+
+        public void SetCurrentState(State<T> s)
+        {
+            _currentState = s;
+        }
+
+        public void SetGlobalState(State<T> s)
+        {
+            _globalState = s;
+        }
+
+        public void SetPreviousState(State<T> s)
+        {
+            _previousState = s;
+        }
+
+        public void Update(double deltaTime)
+        {
+            if (_globalState != null)
+            {
+                _globalState.Execute(_owner, deltaTime);
+            }
+
+            if (_currentState != null)
+            {
+                _currentState.Execute(_owner, deltaTime);
+            }
+        }
+
+        public void ChangeState(State<T> newState)
+        {
+            Debug.Assert(newState != null);
+
+            _previousState = _currentState;
+            _currentState.Exit(_owner);
+            _currentState = newState;
+            _currentState.Enter(_owner);
+        }
+
+        public void RevertToPreviousState()
+        {
+        }
+    }
+}
